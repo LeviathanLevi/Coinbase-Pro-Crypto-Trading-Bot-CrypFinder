60c1c5261fd2454968314880539eb3ba8406ec51
George H. Peden <george@peden.ws>
2021-11-20 14:19:04 -0800

    select trading strategy from .env

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 /* eslint-disable no-unused-vars */
+const pino = require("pino");
+const logger = pino({ level: process.env.LOG_LEVEL || "info" });
 
 /*
 *   This is the entry point of program. Select the strategy or analyzer(s)
@@ -11,10 +13,15 @@ const momentumWithStopLossStrategyStart = require("./strategies/momentumTradingW
 const reverseMomentumStrategyStart = require("./strategies/reverseMomentumTrading/reverseMomentumTrading");
 const reverseMomentumStrategyAnalyzerStart = require("./strategies/reverseMomentumTrading/reverseMomentumTradingAnalyzer");
 
+const strategy = process.env.TRADING_STRATEGY || 'momentum';
+
+logger.info(`Selected ${strategy} strategy`)
 
 /*** Make sure to configure the momentumStrategy in ./strategies/momentumTrading/momentumTrading.js or in the .env before launching ***/
-//Launches the momentum strategy and starts the bot:
-momentumStrategyStart();
+if (strategy == 'momentum') {
+    // Launches the momentum strategy and starts the bot:
+    momentumStrategyStart();
+}
 
 //Launches the momentum strategy anaylzer for back testing:
 //momentumStrategyAnalyzerStart();
@@ -22,8 +29,10 @@ momentumStrategyStart();
 // **********************************************************************************************************************
 
 /*** Make sure to configure the momentumStrategy in ./strategies/momentumTrading/momentumTrading.js or in the .env before launching ***/
-//Launches the reverse momentum strategy and starts the bot:
-//reverseMomentumStrategyStart();
+if (strategy == 'reverse') {
+    //Launches the reverse momentum strategy and starts the bot:
+    reverseMomentumStrategyStart();
+}
 
 //Launches the reverse momentum strategy anaylzer for back testing:
 //reverseMomentumStrategyAnalyzerStart();
@@ -31,5 +40,8 @@ momentumStrategyStart();
 // **********************************************************************************************************************
 
 /*** Make sure to configure the momentumWithStopLossStrategy in ./strategies/momentumTradingWithStopLoss/momentumTradingWithStopLoss.js or in the .env before launching ***/
-//Launches the momentum with stop loss strategy and starts the bot:
-//momentumWithStopLossStrategyStart();
+if (strategy == 'stoploss') {
+    // Launches the momentum with stop loss strategy and starts the bot:
+    logger.debug(`Starting stop loss strategy`);
+    momentumWithStopLossStrategyStart();
+}
--- strategies/reverseMomentumTrading/reverseMomentumTrading.js
+++ strategies/reverseMomentumTrading/reverseMomentumTrading.js
@@ -357,6 +357,7 @@ async function returnHighestFee() {
  * balance to be traded with. Then it calls gainPosition or losePosition appropiately and waits for them to finish and repeats.
  */
 async function reverseMomentumStrategyStart() {
+    logger.debug(`Starting reverse momentum strategy`);
     try {
         logger.info(`Configuration:\napiURI: ${apiURI}\nwebsocketURI: ${websocketURI}\nsellPositionDelta: ${sellPositionDelta}\nbuyPositionDelta: ${buyPositionDelta}\norderPriceDelta: ${orderPriceDelta}\nbaseCurrencyName: ${baseCurrencyName}\nquoteCurrencyName: ${quoteCurrencyName}\ntradingProfileName: ${tradingProfileName}\ndepositProfileName: ${depositProfileName}\ndepositingEnabled: ${depositingEnabled}\ndepositingAmount: ${depositingAmount}\nbalanceMinimum: ${balanceMinimum}`);
 
